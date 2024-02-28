25738825af2a86524af458afe07cb843640277c3
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-02-27 23:14:06 -0300

    mostly all

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,20 +1,57 @@
 import { test, expect } from "@playwright/test";
 
-test("has title", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+const routes = [
+  "/",
+  "admin/administrar_llamados",
+  "admin/llamados/editar/1",
+  "admin/nuevo_llamado",
+  "api/user",
+  "calificar_postulacion/1",
+  "descargar_curriculum/1",
+  "generar_orden_de_merito/1",
+  "jefe_catedra/postulaciones",
+  "login",
+  "password/confirm",
+  "password/reset",
+  "password/reset/1",
+  "postulaciones",
+  "postulaciones/create",
+  "postulaciones/create/1",
+  "postulaciones/1",
+  "postulaciones/1/edit",
+  "register",
+  "sanctum/csrf-cookie",
+  "test/1",
+  "users",
+  "users/create",
+  "users/1",
+  "users/1/edit",
+  "vacantes_cerradas",
+  "vacantes_mi_catedra",
+  "1/postulaciones",
+];
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+const base_url = "http://localhost:8000/";
+for (let route of routes) {
+  test("html " + route, async ({ page }) => {
+    await page.goto("https://validator.w3.org/nu/#textarea");
 
-test("get started link", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+    const html = await fetch(base_url + route).then((x) => x.text());
+    await page.$("textarea").then((x) => x?.fill(html));
 
-  // Click the get started link.
-  await page.getByRole("link", { name: "Get started" }).click();
+    const btn = await page.$("#submit");
+    expect(btn).toBeTruthy();
+    await btn?.click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(
-    page.getByRole("heading", { name: "Installation" }),
-  ).toBeVisible();
-});
+    await page.setViewportSize({
+      width: 1280 * 2,
+      height: 800 * 2,
+    });
+
+    const file_name = route.replace(new RegExp("/", "g"), "-");
+    const path = `out/${file_name}.png`;
+    await page.screenshot({ path });
+
+    expect(1 + 1).toBe(2);
+  });
+}
